216c7d715255d0a86bc55878e213031692c079c8
Josh Gaughan <jdgaugha@iu.edu>
2025-10-27 16:35:32 +0000

    Exercise 1 Part 2

--- apputil.py
+++ apputil.py
@@ -10,7 +10,15 @@ class GroupEstimate(object):
         self.estimate = estimate.lower()
     
     def fit(self, X, y):
-        return None
+        X_columns = list(X.columns)
+
+        y_series = pd.Series(y, name = "target_value", index = X.index)
+        data = pd.concat([X, y_series], axis = 1)
+
+        agg_func = np.mean if self.estimate == 'mean' else np.median
+
+        self.group_map = data.groupby(X_columns)['target_value'].agg(agg_func).to_dict
+        return self
 
     def predict(self, X):
         return None

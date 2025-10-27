7d6acfde533d3b848e3fb3873c6bcf0f250e445e
Josh Gaughan <jdgaugha@iu.edu>
2025-10-27 16:01:47 +0000

    Exercise 1 Part 1

--- apputil.py
+++ apputil.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 def GroupEstimate(object):
     def __init__(self, estimate):
-        self.estimate = estimate
+        valid_estimate = ['mean', 'median']
+        if estimate.lower() not in valid_estimate:
+            raise ValueError(f'Estimate must be one of {valid_estimate}')
+
+        self.estimate = estimate.lower()
     
     def fit(self, X, y):
         return None

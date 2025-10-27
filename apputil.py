c155fef6b88a6d2b8cc6b5f3847579e562171f74
Josh Gaughan <jdgaugha@iu.edu>
2025-10-27 17:46:00 +0000

    Exercise 1 Part 3 Comments and Docstring

--- apputil.py
+++ apputil.py
@@ -13,19 +13,19 @@ class GroupEstimate(object):
         No return value
         '''
 
-        # Define what are valid estimate inputs
+        # Define what are valid estimate inputs.
         valid_estimate = ['mean', 'median']
 
-        # Raise an error if the estimate input isn't valid
+        # Raise an error if the estimate input isn't valid.
         if estimate.lower() not in valid_estimate:
             raise ValueError(f'Estimate must be one of {valid_estimate}')
 
-        # Assign the estimate to the 'self' object
+        # Assign the estimate to the 'self' object.
         self.estimate = estimate.lower()
     
     def fit(self, X, y):
         '''
-        Method that takes in 
+        Method that takes in data, merges it and modifies it.
 
         Parameters:
         X (pandas.DataFrame): Contains categorical data
@@ -35,14 +35,14 @@ class GroupEstimate(object):
         self: Technically not necessary to answer the question
         but included to make things look complete.
         '''
-        # If y input is a list, convert it to an array
+        # If y input is a list, convert it to an array.
         if isinstance(y, list):
             y = np.array(y)
 
-        # Columns in X are assigned to the features of the object
+        # Columns in X are assigned to the features of the object.
         self.features = list(X.columns)
 
-        # Merging X and y together into one
+        # Merging X and y together into one.
         y_series = pd.Series(y, name = "target_value", index = X.index)
         data = pd.concat([X, y_series], axis = 1)
 
@@ -56,17 +56,36 @@ class GroupEstimate(object):
         return self
 
     def predict(self, X_):
+        '''
+        Determines which group the array of observations in X_
+        fall into.
+
+        Parameters:
+        X_ (np.array or pandas.DataFrame): An array or dataframe
+        of values to be used to produce the return value.
+
+        Return value:
+        predictions_array (np.array): An np.array of corresponding
+        estimates for y.
+        '''
+        
+        # If X_ is a list or array, make it a pandas dataframe.
         if isinstance(X_, (list, np.ndarray)):
             X_ = pd.DataFrame(X_, columns = self.features)
 
+        # Create the group keys.
         group_keys = [tuple(row) for row in X_.values]
 
+        # Create predictions.
         predictions = [self.group_map.get(key, np.nan) for key in group_keys]
 
+        # Convert the predictions into an array.
         predictions_array = np.array(predictions)
 
+        # Find the amount of missing category or categories.
         missing_count = np.sum(np.isnan(predictions_array))
 
+        # If the count is more than zero, print a message.
         if missing_count > 0:
             print(f'There are {missing_count} observation contained group(s) that were assigned to NaN')
 
